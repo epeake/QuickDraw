9ecc658bb6d09cb2433c874b05952792ecb90480
“[Elijah <elijah.peake@gmail.com>
2018-11-26 20:12:44 -0500

    using built-in conv layers

--- QuickDrawModel.py
+++ QuickDrawModel.py
@@ -21,40 +21,6 @@ n_outputs = len(label_to_class)
 csv_len = int(check_output('wc -l ' + DIRPATH + 'train.csv | grep -o "[0-9]\+"', shell=True))
 
 
-def conv_layer_vgg(input, chanels_in, chanels_out, scope_name):
-    """
-    Convolutional layer for VGGNet16
-
-    :param input: input layer
-    :param chanels_in: (int)
-    :param chanels_out: (int)
-    :return: activation layer
-    """
-    with tf.name_scope(scope_name):
-        W = tf.get_variable([3, 3, chanels_in, chanels_out], initializer=tf.contrib.layers.xavier_initializer(), name="W")
-        b = tf.Variable(tf.constant(0.1, shape=[chanels_out]), name="B")
-        conv = tf.nn.conv2d(input, W, strides=[1, 1, 1, 1], padding="SAME")
-        active = tf.nn.relu(conv + b)
-        return active
-
-
-def conv_layer(input, filter_size, chanels_in, chanels_out, stride, pad, scope_name):
-    """
-    Convolutional layer for VGGNet16
-
-    :param input: input layer
-    :param chanels_in: (int)
-    :param chanels_out: (int)
-    :return: activation layer
-    """
-    with tf.name_scope(scope_name):
-        W = tf.get_variable([filter_size, filter_size, chanels_in, chanels_out], initializer=tf.contrib.layers.xavier_initializer(), name="W")
-        b = tf.Variable(tf.constant(0.1, shape=[chanels_out]), name="B")
-        conv = tf.nn.conv2d(input, W, strides=[1, stride, stride, 1], padding=pad)
-        active = tf.nn.relu(conv + b)
-        return active
-
-
 def cnn_model(model_type, l_r):
     learning_rate = l_r
 
@@ -63,27 +29,53 @@ def cnn_model(model_type, l_r):
             X = tf.placeholder("float", [None, HEIGHT, WIDTH, 1])
             Y = tf.placeholder("float", [None, n_outputs])
 
-            conv_1_1 = conv_layer_vgg(X, 1, 64, "conv_1_1")
-            conv_1_2 = conv_layer_vgg(conv_1_1, 64, 64, "conv_1_2")
+            conv_1_1 = tf.layers.conv2d(X, filters=64, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_1_1")
+            conv_1_2 = tf.layers.conv2d(conv_1_1, filters=64, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_1_2")
             pool1 = tf.nn.max_pool(conv_1_2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name="pool1")
 
-            conv_2_1 = conv_layer_vgg(pool1, 64, 128, "conv_2_1")
-            conv_2_2 = conv_layer_vgg(conv_2_1, 128, 128, "conv_2_2")
+            conv_2_1 = tf.layers.conv2d(pool1, filters=128, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_2_1")
+            conv_2_2 = tf.layers.conv2d(conv_2_1, filters=128, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_2_2")
             pool2 = tf.nn.max_pool(conv_2_2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name="pool2")
 
-            conv_3_1 = conv_layer_vgg(pool2, 128, 256, "conv_3_1")
-            conv_3_2 = conv_layer_vgg(conv_3_1, 256, 256, "conv_3_2")
-            conv_3_3 = conv_layer_vgg(conv_3_2, 256, 256, "conv_3_3")
+            conv_3_1 = tf.layers.conv2d(pool2, filters=256, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_3_1")
+            conv_3_2 = tf.layers.conv2d(conv_3_1, filters=256, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_3_2")
+            conv_3_3 = tf.layers.conv2d(conv_3_2, filters=256, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_3_3")
             pool3 = tf.nn.max_pool(conv_3_3, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name="pool3")
 
-            conv_4_1 = conv_layer_vgg(pool3, 256, 512, "conv_4_1")
-            conv_4_2 = conv_layer_vgg(conv_4_1, 512, 512, "conv_4_2")
-            conv_4_3 = conv_layer_vgg(conv_4_2, 512, 512, "conv_4_3")
+            conv_4_1 = tf.layers.conv2d(pool3, filters=512, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_4_1")
+            conv_4_2 = tf.layers.conv2d(conv_4_1, filters=512, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_4_2")
+            conv_4_3 = tf.layers.conv2d(conv_4_2, filters=512, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_4_3")
             pool4 = tf.nn.max_pool(conv_4_3, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name="pool4")
 
-            conv_5_1 = conv_layer_vgg(pool4, 512, 512, "conv_5_1")
-            conv_5_2 = conv_layer_vgg(conv_5_1, 512, 512, "conv_5_2")
-            conv_5_3 = conv_layer_vgg(conv_5_2, 512, 512, "conv_5_3")
+            conv_5_1 = tf.layers.conv2d(pool4, filters=512, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_5_1")
+            conv_5_2 = tf.layers.conv2d(conv_5_1, filters=512, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_5_2")
+            conv_5_3 = tf.layers.conv2d(conv_5_2, filters=512, kernel_size=3,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_5_3")
             pool5 = tf.nn.max_pool(conv_5_3, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name="pool5")
 
             pre_fully_connected = tf.contrib.layers.flatten(pool5, scope="flattened")
@@ -102,15 +94,25 @@ def cnn_model(model_type, l_r):
             X = tf.placeholder("float", [None, HEIGHT, WIDTH, 1])
             Y = tf.placeholder("float", [None, n_outputs])
 
-            conv_1_1 = conv_layer(X, 11, 1, 96, 4, "VALID", "conv_1_1")
+            conv_1_1 = tf.layers.conv2d(X, filters=96, kernel_size=11, strides=(4, 4),
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="VALID",
+                                        activation=tf.nn.relu, name="conv_1_1")
             pool1 = tf.nn.max_pool(conv_1_1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name="pool1")
 
-            conv_2_1 = conv_layer(pool1, 5, 96, 256, 1, "SAME", "conv_2_1")
+            conv_2_1 = tf.layers.conv2d(pool1, filters=256, kernel_size=5, strides=(1, 1),
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="VALID",
+                                        activation=tf.nn.relu, name="conv_2_1")
             pool2 = tf.nn.max_pool(conv_2_1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='Same', name="pool2")
 
-            conv_3_1 = conv_layer(pool2, 3, 256, 348, 1, "SAME", "conv_3_1")
-            conv_3_2 = conv_layer(conv_3_1, 3, 348, 348, 1, "SAME", "conv_3_2")
-            conv_3_3 = conv_layer(conv_3_2, 3, 96, 256, 1, "SAME", "conv_3_3")
+            conv_3_1 = tf.layers.conv2d(pool2, filters=348, kernel_size=3, strides=(1, 1),
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_3_1")
+            conv_3_2 = tf.layers.conv2d(conv_3_1, filters=348, kernel_size=3, strides=(1, 1),
+                             kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                             activation=tf.nn.relu, name="conv_3_2")
+            conv_3_3 = tf.layers.conv2d(conv_3_2, filters=256, kernel_size=3, strides=(1, 1),
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer(), padding="SAME",
+                                        activation=tf.nn.relu, name="conv_3_3")
             pool3 = tf.nn.max_pool(conv_3_3, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name="pool3")
 
             pre_fully_connected = tf.contrib.layers.flatten(pool3, scope="flattened")
