ccd1fb8c630cf9739a22ba1afa2cab1cb1ac4e58
“[Elijah <elijah.peake@gmail.com>
2018-11-28 11:59:26 -0500

    cleaned shell script

--- merge_doodles.sh
+++ merge_doodles.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-if [ $(find "/data/scratch/epeake/Google-Doodles/" -name "train.csv") ]
+f_path=$"/data/scratch/epeake/Google-Doodles/"
+python_env=$"/home/epeake/venv/mainenv/bin/python3"
+
+if [ $(find $f_path -name "train.csv") ]
   then
-    rm /data/scratch/epeake/Google-Doodles/train.csv
-    rm /data/scratch/epeake/Google-Doodles/cross_validate.csv
+    rm $f_path"train.csv"
+    rm $f_path"cross_validate.csv"
 fi
 
-/home/epeake/venv/mainenv/bin/python3 merge_doodles.py /data/scratch/epeake/Google-Doodles/
+$python_env merge_doodles.py $f_path
 
 # split train and test 80% / 20%
-n_lines=$(wc -l /data/scratch/epeake/Google-Doodles/all_doodles.csv | grep -o "[0-9]\+")
+n_lines=$(wc -l $f_path"all_doodles.csv" | grep -o "[0-9]\+")
 n_train=$(expr $n_lines \* 8 / 10)
 
-split -l $n_train /data/scratch/epeake/Google-Doodles/all_doodles.csv /data/scratch/epeake/Google-Doodles/train
-mv /data/scratch/epeake/Google-Doodles/trainaa /data/scratch/epeake/Google-Doodles/train.csv
-mv /data/scratch/epeake/Google-Doodles/trainab /data/scratch/epeake/Google-Doodles/cross_validate.csv
+split -l $n_train $f_path"all_doodles.csv" $f_path"train"
+mv $f_path"trainaa" $f_path"train.csv"
+mv $f_path"trainab" $f_path"cross_validate.csv"
 
-rm /data/scratch/epeake/Google-Doodles/all_doodles.csv
+rm $f_path"all_doodles.csv"
+
